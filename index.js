9e0c11eee8e3fba544ea5980d1b87741b90d2668
techoneel <nilotpal.saha00@gmail.com>
2022-03-22 11:10:34 +0530

    + exported few components

---
+++ index.js
@@ -0,0 +1,14 @@
+import CoreClasses from "./CoreClasses";
+import CoreView from "./components/layout/CoreView";
+import Container from "./components/layout/Container";
+import Column from "./components/layout/Column";
+import Row from "./components/layout/Row";
+import { View } from "react-native";
+
+export { CoreClasses, CoreView };
+// module.exports = { CoreView };
+// module.exports = { Container };
+// module.exports = { Column };
+// module.exports = { Row };
+
+export class TestView extends View {}
